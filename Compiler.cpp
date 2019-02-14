a21cefb80d58cd6a79bf101020093b784b253e35
Alexey Gladkov <alexxX249@gmail.com>
2019-02-15 01:41:04 +0300

    feature: new output file format.
    old format moved to a dump file. (program as text)
    new format: binary
             0:[dataSize]-8bytes
             8:[data]
    dataSize+8:[program]
    
    feature: added lib to parse cmd args.

--- Compiler.cpp
+++ Compiler.cpp
@@ -3,6 +3,9 @@
 #include "ErrorHandling.h"
 #include "CodeGenerator.h"
 
+#include "optparse.h"
+
+#include <fstream>
 #include <cstdio>
 #include <cstdlib>
 extern int yylex();
@@ -23,9 +26,58 @@ namespace
     }
 }
 
-void Compiler::Compile(const char* sourceFile, const char* outputFile)
+std::vector<char> Compiler::ReadDataFile(const std::string& path)
 {
-    AST::AbstractSyntaxTree ast = Parse(sourceFile);
+    std::ifstream f{ path, std::ios::binary | std::ios::ate };
+    
+    if (f.is_open())
+    {
+        const size_t size = f.tellg();
+        if (size != 0)
+        {
+            f.seekg(std::ios::beg);
+
+            std::vector<char> data;
+            data.resize(size);
+
+            f.read(data.data(), size);
+
+            return data;
+        }
+        else
+            std::cerr << "Can't read a data file: file is empty.\n";
+    }
+    else
+        std::cerr << "Can't read the data file: can't open the data file.\n";
+
+    exit(-1);
+}
+
+void Compiler::Compile(int argc, char** argv)
+{
+    optparse::OptionParser parser = optparse::OptionParser().description("MACRO11 compiler");
+
+    parser.add_option("-i").help("input file.").dest("input");
+    parser.add_option("-o").help("output file.").dest("out");
+    parser.add_option("-d").help("data file.").dest("data");
+
+    const optparse::Values options = parser.parse_args(argc, argv);
+    if (options.is_set("input") == false || options.is_set("out") == false)
+    {
+        parser.print_help();
+        exit(-1);
+    }
+
+    uint64_t dataSize = 0;
+    std::vector<char> data;
+    if (options.is_set("data"))
+    {
+        data = ReadDataFile(options["data"]);
+        dataSize = data.size();
+    }
+
+    const std::string& sourceFile = options["input"];
+    AST::AbstractSyntaxTree ast = Parse(sourceFile.c_str());
     
     AST::SemanticAnalyzer sa;
     ast.Accept(&sa);
@@ -35,13 +87,21 @@ void Compiler::Compile(const char* sourceFile, const char* outputFile)
     const std::vector<Word>& program = codeGen.Generate(&ast);
     DumpErrors(codeGen.GetErrors());
     
-    FILE* f = fopen(outputFile, "w");
-
+    FILE* f = fopen("dump.txt", "w");
     for (const Word instruction : program)
     {
         std::fprintf(f, "%07o\n", instruction);
     }
-    
+    fclose(f);
+
+    uint64_t programSize = program.size() * sizeof(Word);
+
+    const std::string& outputFile = options["out"];
+    f = fopen(outputFile.c_str(), "w");
+    fwrite(&dataSize, sizeof(uint64_t), 1, f);
+    fwrite(data.data(), sizeof(Byte), dataSize, f);
+    fwrite(program.data(), sizeof(Byte), programSize, f);
+
     fclose(f);
 }
 
@@ -66,14 +126,6 @@ AST::AbstractSyntaxTree Compiler::Parse(const char* sourceFile) const
 
 int main(int argc, char** argv)
 {
-    if (argc != 3)
-    {
-        fprintf(stderr, "input: {source} {output}");
-        exit(-1);
-    }
-    
-    //yydebug = 1;
-
     Compiler c;
-    c.Compile(argv[1], argv[2]);
+    c.Compile(argc, argv);
 }
--- Compiler.h
+++ Compiler.h
@@ -5,8 +5,12 @@
 class Compiler
 {
 public:
-    void Compile(const char* sourceFile, const char* outputFile);
+    void Compile(int argc, char** argv);
 
 private:
+    std::vector<char> ReadDataFile(const std::string& path);
+
+private:
+
     AST::AbstractSyntaxTree Parse(const char* sourceFile) const;
 };
